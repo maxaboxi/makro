c5e4eaf71fa78412262a3bbac9f026fd141ddde9
maxaboxi <arto.tukki@gmail.com>
2018-12-09 15:19:23 +0200

    Removed unused logger

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,19 +1,4 @@
 const mongoose = require('mongoose');
-const winston = require('winston');
-const path = require('path');
-
-const tsFormat = () =>
-  new Date().toLocaleDateString() + ' - ' + new Date().toLocaleTimeString();
-
-const logger = winston.createLogger({
-  level: 'error',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({
-      filename: path.join(__dirname, '../logs', 'user.error.log')
-    })
-  ]
-});
 
 const UserSchema = mongoose.Schema(
   {
